7ae949b4e20b023dfc63471ec014c71f44400229
Harry Watkins <harry.jw@outlook.com>
2024-06-14 22:51:56 +0100

    Convert data to tensors

--- house-price-predictor.py
+++ house-price-predictor.py
@@ -1,3 +1,14 @@
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-df = pd.read_csv('Housing.csv')
+df = pd.read_csv('Housing.csv', delimiter=',')
+y = df.iloc[:,0]
+X = df.iloc[:,1:-1]
+
+X = torch.tensor(X.values, dtype=torch.float32)
+y = torch.tensor(y.values, dtype=torch.float32).reshape(-1, 1)
+
+print(X)
+print(y)
