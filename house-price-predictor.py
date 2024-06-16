ce3d340dde2be833819718a038c5a632ae7b441e
Harry Watkins <harry.jw@outlook.com>
2024-06-16 22:33:30 +0100

    Map csv non integers to integers

--- house-price-predictor.py
+++ house-price-predictor.py
@@ -3,15 +3,17 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+yes_no_columns = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea']
 yes_no_conversion = {'yes': 1, 'no': 0}
 furnishing_conversion = {'furnished': 3, 'semi-furnished': 2, 'unfurnished': 1}
 
 df = pd.read_csv('Housing.csv', delimiter=',')
+for column in yes_no_columns:
+    df[column] = df[column].map(yes_no_conversion)
+df['furnishingstatus'] = df['furnishingstatus'].map(furnishing_conversion)
+
 y = df.iloc[:,0]
 X = df.iloc[:,1:-1]
 
 X = torch.tensor(X.values, dtype=torch.float32)
 y = torch.tensor(y.values, dtype=torch.float32).reshape(-1, 1)
-
-print(X)
-print(y)
