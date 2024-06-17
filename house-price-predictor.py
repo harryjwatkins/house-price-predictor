88521aabc6cb6f68dab13250bb806305ba57ea96
Harry Watkins <harry.jw@outlook.com>
2024-06-17 23:25:53 +0100

    Add model class

--- house-price-predictor.py
+++ house-price-predictor.py
@@ -3,6 +3,25 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+class PimaClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden1 = nn.Linear(12, 12)
+        self.act1 = nn.ReLU()
+        self.hidden2 = nn.Linear(12, 12)
+        self.act2 = nn.ReLU()
+        self.output = nn.Linear(12, 1)
+        self.act_output = nn.Sigmoid()
+ 
+    def forward(self, x):
+        x = self.act1(self.hidden1(x))
+        x = self.act2(self.hidden2(x))
+        x = self.act_output(self.output(x))
+        return x
+    
+model = PimaClassifier()
+print(model)
+
 yes_no_columns = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea']
 yes_no_conversion = {'yes': 1, 'no': 0}
 furnishing_conversion = {'furnished': 3, 'semi-furnished': 2, 'unfurnished': 1}
