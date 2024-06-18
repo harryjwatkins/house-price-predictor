cf30952db493689526842a41b13063b85f27d7ce
Harry Watkins <harry.jw@outlook.com>
2024-06-18 22:30:19 +0100

    Add training (needs to be fixed)

--- house-price-predictor.py
+++ house-price-predictor.py
@@ -18,9 +18,6 @@ class PimaClassifier(nn.Module):
         x = self.act2(self.hidden2(x))
         x = self.act_output(self.output(x))
         return x
-    
-model = PimaClassifier()
-print(model)
 
 yes_no_columns = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea']
 yes_no_conversion = {'yes': 1, 'no': 0}
@@ -36,3 +33,24 @@ X = df.iloc[:,1:-1]
 
 X = torch.tensor(X.values, dtype=torch.float32)
 y = torch.tensor(y.values, dtype=torch.float32).reshape(-1, 1)
+
+model = PimaClassifier()
+
+loss_fn = nn.BCELoss()  # binary cross entropy
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+number_of_epochs = 100
+batch_size = 10
+
+for epoch in range(number_of_epochs):
+    for i in range(0, len(X), batch_size):
+        Xbatch = X[i:i+batch_size]
+        y_pred = model(Xbatch)
+        ybatch = y[i:i+batch_size]
+        loss = loss_fn(y_pred, ybatch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Finished epoch {epoch}, latest loss {loss}')
+
+
